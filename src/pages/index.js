86573689b2e8ccec5c72aac510437c440cb0f8a4
hwkim-dev <k1h6w4@gmail.com>
2026-04-14 11:46:32 +0900

    Create Home page with introduction and contact sections.

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+# Welcome to My Homepage
+
+## Introduction
+Welcome to my personal homepage! Here you'll find my blogs, research papers, and more.
+
+## Contact
+Feel free to reach out through my email: example@example.com
